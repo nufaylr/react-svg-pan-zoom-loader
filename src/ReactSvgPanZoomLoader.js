076d5e7959065e3358094fb635ab38e10148a908
jalvarez957 <87673004+KilloPillers@users.noreply.github.com>
2023-07-03 14:58:50 -0700

    configured SvgReactPanZoomLoader to take the width and height of its parent container

--- src/ReactSvgPanZoomLoader.js
+++ src/ReactSvgPanZoomLoader.js
@@ -10,7 +10,7 @@ import { SvgLoader } from 'react-svgmt'
  */
 const ReactSvgPanZoomLoader = (props) => {
     return (
-        <div>
+        <div style={{width:'100%', height:'100%'}}>
             {props.render(
                 <SvgLoader path={props.src} svgXML={props.svgXML}>
                     {props.proxy}
